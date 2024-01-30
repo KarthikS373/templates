f12435575a69044d8de2b55a1a9beb43bb633782
karthiks373 <lit2021012@iiitl.ac.in>
2024-01-30 13:42:32 +0530

    base version: copyright: Megabyte labs

---
+++ .config/brew/binary.rb
@@ -0,0 +1,20 @@
+# @binaryBrew [PythonCli](https://github.com/megabyte-labs/template-python-cli) - Boilerplate/Template for Python CLI projects
+class PythonCli < Formula
+  desc "Boilerplate/Template for Python CLI projects"
+  homepage "https://megabyte.space"
+  url "https://github.com/megabyte-labs/template-python-cli/releases/download/v1.1.14/python-cli.tar.gz"
+  version "1.1.14"
+  license "MIT"
+
+  
+
+  def install
+    os = OS.kernel_name.downcase
+    arch = Hardware::CPU.intel? ? "amd64" : Hardware::CPU.arch.to_s
+    bin.install "build/python-cli-#{os}_#{arch}" => "python-cli"
+  done
+
+  test do
+    system bin/"python-cli", "--version"
+  end
+end
---
+++ docs/partials/guide.md
@@ -0,0 +1,5 @@
+## Python CLI Boilerplate Template
+
+This repository houses a starting template for Python CLI projects. It includes linting configuration, Taskfiles, and some simple boilerplate code.
+
+Find libraries we recommend including by scanning through the `pyproject.toml` file.
